62de889f8df4b23e19d60130c3cae95ba477be16
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-11-01 19:50:22 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+// Middleware for CORS
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// Middleware for parsing JSON
+app.use(express.json());
+
+// ✅ Connect to MongoDB
+connectDB();
+
+app.use("/api/v1/auth", authRoutes);
+
+//serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
