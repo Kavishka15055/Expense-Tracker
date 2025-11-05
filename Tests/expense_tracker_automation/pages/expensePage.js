537b0ac4416b6efaf26f62c45991084684ed89c3
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-11-05 18:19:27 +0530

    Update expensePage.js

--- Tests/expense_tracker_automation/pages/expensePage.js
+++ Tests/expense_tracker_automation/pages/expensePage.js
@@ -21,6 +21,23 @@ export default class ExpensePage {
     await this.driver.wait(until.elementIsVisible(addBtn), 5000);
     await addBtn.click();
 
+    //Fill Income Icone
+      const iconBtn = await this.driver.wait(
+      until.elementLocated(By.xpath('//*[@id="root"]/div[1]/div/div[2]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/p')),
+      10000
+    );
+    await this.driver.wait(until.elementIsEnabled(iconBtn), 5000);
+    await iconBtn.click();
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    const iconSelector = await this.driver.wait(
+      until.elementLocated(By.xpath('//*[@id="root"]/div[1]/div/div[2]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div[2]/aside/div[2]/ul/li[3]/div/button[32]/img')),
+      10000
+    );
+    await this.driver.wait(until.elementIsEnabled(iconSelector), 5000);
+    await iconSelector.click();
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
     // Fill expense name
     const categoryField = await this.driver.wait(
       until.elementLocated(By.xpath('//*[@id="root"]/div[1]/div/div[2]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/input')),
@@ -28,6 +45,7 @@ export default class ExpensePage {
     );
     await this.driver.wait(until.elementIsVisible(categoryField), 5000);
     await categoryField.sendKeys(category);
+    await new Promise(resolve => setTimeout(resolve, 1000));
 
     // Fill expense amount
     const amountField = await this.driver.wait(
@@ -36,6 +54,7 @@ export default class ExpensePage {
     );
     await this.driver.wait(until.elementIsVisible(amountField), 5000);
     await amountField.sendKeys(amount);
+    await new Promise(resolve => setTimeout(resolve, 1000));
 
     // Fill expense date
     const dateField = await this.driver.wait(
@@ -44,6 +63,7 @@ export default class ExpensePage {
     );
     await this.driver.wait(until.elementIsVisible(dateField), 5000);
     await dateField.sendKeys(date);
+    await new Promise(resolve => setTimeout(resolve, 1000));
 
     // Click submit button
     const submitBtn = await this.driver.wait(
